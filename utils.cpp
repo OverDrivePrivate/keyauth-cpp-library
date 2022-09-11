67df341757b41a3e95bdbb47ee2f381d778e1be0
OverDrive <108163773+OverDrivePrivate@users.noreply.github.com>
2022-09-11 02:56:28 -0400

    adjusted hwid lock
    added a different system to hwid lock, uses procid. (i really dont care if this is "insecure", i just didnt want to reset users everytime they fac reset and its decent enough)

--- utils.cpp
+++ utils.cpp
@@ -1,13 +1,33 @@
 #include "utils.hpp"
-
+#include <array>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include<conio.h>
 #include <atlsecurity.h> 
+#include <string>
+
+std::string GetProcessorId() {
+	std::array<int, 4> cpuInfo;
+	__cpuid(cpuInfo.data(), 1);
+	std::ostringstream buffer;
+	buffer
+		<< std::uppercase << std::hex << std::setfill('0')
+		<< std::setw(8) << cpuInfo.at(3)
+		<< std::setw(8) << cpuInfo.at(0);
+	return buffer.str();
+}
 
 std::string utils::get_hwid() {
 	ATL::CAccessToken accessToken;
 	ATL::CSid currentUserSid;
-	if (accessToken.GetProcessToken(TOKEN_READ | TOKEN_QUERY) &&
+	std::string thingie = GetProcessorId();
+	if (!thingie.empty())
+		return thingie;
+	/*if (accessToken.GetProcessToken(TOKEN_READ | TOKEN_QUERY) &&
 		accessToken.GetUser(&currentUserSid))
-		return std::string(CT2A(currentUserSid.Sid()));
+		return std::string(CT2A(currentUserSid.Sid()));*/
 	return "none";
 }
 
